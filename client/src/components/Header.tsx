e2ea45f5e01a1f6e51de3366dacdd833c1a13bbf
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-07 10:54:18 -0800

    set up tailwind css - radical

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return(
-        <header>
+        <header className="text-3xl font-bold underline">
             App Tracker
         </header>
     );
