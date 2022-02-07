3cd5db7b4b77ae46e7dd4a63f235b4c4b2fbbcc9
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-07 11:42:29 -0800

    basic form showing up needs to be edited :); main el is displaying correctly now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,16 @@ import './App.css';
 // components
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import TrackerForm from './components/TrackerForm';
 
 function App() {
   return (
     <div className='h-screen'>
       <Header />
-      <NavBar />
+      <main className='h-screen flex'>
+        <NavBar />
+        <TrackerForm />
+      </main>
     </div>
   );
 }
