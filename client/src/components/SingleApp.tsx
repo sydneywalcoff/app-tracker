e9e1e6149c7466f6f7cc9a852405e990d631d6fb
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-09 18:17:53 -0800

    add edit button; no functionality yet

--- client/src/components/SingleApp.tsx
+++ client/src/components/SingleApp.tsx
@@ -4,30 +4,34 @@ import { useParams } from 'react-router-dom';
 
 const SingleApp = () => {
     let { jobId } = useParams();
-    if(!jobId) {
+    if (!jobId) {
         return (
             <h1 className="mx-auto my-8 text-3xl">Sorry! Something went wrong. </h1>
         );
     }
     let job = getSingleJob(jobId);
-    if(!job) {
+    if (!job) {
         return (
             <h1 className="mx-auto my-8 text-3xl">Sorry! No application with that ID. </h1>
         );
     }
     return (
-        <section className='flex p-9 w-screen justify-center'>
-            <div className="px-5 basis-3/4">
-                <h1 className='text-4xl font-bold mb-2'>{job.jobTitle}</h1>
-                <h2 className="text-xl mb-5">{job.companyName}</h2>
-                <p className='text-md'>{job.jobDescription}</p>
+        <section className='p-9 w-screen'>
+            <div className="flex justify-center">
+                <div className="px-5 basis-3/4">
+                    <h1 className='text-4xl font-bold mb-2'>{job.jobTitle}</h1>
+                    <h2 className="text-xl mb-5">{job.companyName}</h2>
+                    <p className='text-md'>{job.jobDescription}</p>
+                </div>
+                <div className='px-5 basis-1/4'>
+                    <button type="button" className="inline-flex justify-center mb-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit</button>
+                    <div>
+                        <h2 className='mb-3'><span className="font-bold">location: </span>{job.location} </h2>
+                        <h2 className='mb-3'><span className="font-bold">stage: </span>{job.stage} </h2>
+                        <h2 className='mb-3'><span className="font-bold">date applied: </span>{job.dateApplied} </h2>
+                    </div>
+                </div>
             </div>
-            <div className = 'px-5 basis-1/4'>
-                <h2 className='pb-3'><span className="font-bold">location: </span>{job.location} </h2>
-                <h2 className='pb-3'><span className="font-bold">stage: </span>{job.stage} </h2>
-                <h2 className='pb-3'><span className="font-bold">date applied: </span>{job.dateApplied} </h2>
-            </div>
-
         </section>
     );
 };
