7802d581aef59480def83bdfdb53708190398691
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-25 13:18:58 -0800

    added save button styles

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -3,79 +3,86 @@ import "./styles.css";
 import StageBadge from "../StageBadge";
 
 interface jobProp {
-  id: string;
-  jobTitle: string;
-  companyName: string;
-  jobDescription: string;
-  location: string;
-  stage: string;
-  dateApplied: string;
+    id: string;
+    jobTitle: string;
+    companyName: string;
+    jobDescription: string;
+    location: string;
+    stage: string;
+    dateApplied: string;
 }
 
 interface ModalProps {
-  job: jobProp;
-  setModalOpen: Dispatch<SetStateAction<boolean>>;
+    job: jobProp;
+    setModalOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 const Modal = ({ job, setModalOpen }: ModalProps) => {
-  const {
-    jobTitle,
-    dateApplied,
-    companyName,
-    stage,
-    jobDescription,
-    location,
-  } = job;
-  return (
-    <>
-      <div className="darkBG" />
-      <div className="centered modal p-4">
-        <div className="flex justify-between">
-          <h3 className="text-3xl pl-4">Edit Details:</h3>
-          <p className="flex" onClick={() => setModalOpen(false)}>
-            x
-          </p>
-        </div>
-        <div className="flex p-4">
-          <div className="flex-col basis-3/4 mr-3">
-            <div className="my-3">
-                <label htmlFor="job-title" className="font-bold mr-2 pr-1">Title:</label>
-                <input
-                  name="job-title"
-                  className="mb-2 p-1 border-solid border-2"
-                  placeholder={jobTitle}
-                />
-                <label htmlFor="company-name" className="font-bold m-2 pr-1">Company:</label>
-                <input
-                  className="mb-2 p-1 border-solid border-2"
-                  name="company-name"
-                  placeholder={companyName}
-                />
+    const {
+        jobTitle,
+        dateApplied,
+        companyName,
+        stage,
+        jobDescription,
+        location,
+    } = job;
+    return (
+        <>
+            <div className="darkBG" />
+            <div className="centered modal p-4">
+                <div className="flex justify-between">
+                    <h3 className="text-3xl pl-4">Edit Details:</h3>
+                    <p className="flex" onClick={() => setModalOpen(false)}>
+                        x
+                    </p>
+                </div>
+                <div className="flex p-4">
+                    <div className="flex-col basis-3/4 mr-3">
+                        <div className="my-3">
+                            <label htmlFor="job-title" className="font-bold mr-2 pr-1">
+                                Title:
+                            </label>
+                            <input
+                                name="job-title"
+                                className="mb-2 p-1 border-solid border-2"
+                                placeholder={jobTitle}
+                            />
+                            <label htmlFor="company-name" className="font-bold m-2 pr-1">
+                                Company:
+                            </label>
+                            <input
+                                className="mb-2 p-1 border-solid border-2"
+                                name="company-name"
+                                placeholder={companyName}
+                            />
+                        </div>
+                        <h3 className="font-bold mb-2">Job Description</h3>
+                        <textarea
+                            className="whitespace-pre-wrap p-1 w-full h-full border-solid border-2"
+                            placeholder={jobDescription}
+                        />
+                    </div>
+                    <div className="flex-col basis-1/4">
+                        <div>
+                            <h4 className="mb-3">
+                                <span className="font-bold">date applied: </span>
+                                {dateApplied}{" "}
+                            </h4>
+                            <h4 className="mb-3">
+                                <span className="font-bold">location: </span>
+                                {location}{" "}
+                            </h4>
+                            <h4 className="mb-3">
+                                <span className="font-bold">stage: </span>{" "}
+                                <StageBadge stage={stage} />{" "}
+                            </h4>
+                        </div>
+                        <button type="button"  className="mb-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                    </div>
+                </div>
             </div>
-            <h3 className="font-bold mb-2">Job Description</h3>
-            <textarea
-              className="whitespace-pre-wrap p-1 w-full h-full border-solid border-2"
-              placeholder={jobDescription}
-            />
-          </div>
-          <div className="basis-1/4">
-            <h4 className="mb-3">
-              <span className="font-bold">date applied: </span>
-              {dateApplied}{" "}
-            </h4>
-            <h4 className="mb-3">
-              <span className="font-bold">location: </span>
-              {location}{" "}
-            </h4>
-            <h4 className="mb-3">
-              <span className="font-bold">stage: </span>{" "}
-              <StageBadge stage={stage} />{" "}
-            </h4>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Modal;
