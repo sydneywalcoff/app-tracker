bbeb40554c970f602902a12e8c0d2dcb004b402b
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-25 15:21:48 -0800

    add types for mongoose and typescript-ify the mongoose connection

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT: string | number = process.env.PORT || 3001;
 
 db.once('open', () => {
     app.listen(PORT, () => {
