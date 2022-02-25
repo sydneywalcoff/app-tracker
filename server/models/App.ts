6d19aeffe0f0d9432c813ea28fb8567b02e425c0
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-25 15:31:49 -0800

    App model

---
+++ server/models/App.ts
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+interface App {
+    jobTitle: string,
+    jobDescription: string, 
+    status: string,
+    location: string,
+    quickApply: boolean,
+    jobScore?: number
+}
+
+const appSchema = new Schema({
+    jobTitle: {
+        type: String,
+        require: true
+    },
+    jobDescription:  {
+        type: String,
+        require: true
+    }, 
+    status:  {
+        type: String,
+        require: true
+    },
+    location:  {
+        type: String,
+        require: true
+    },
+    quickApply:  {
+        type: Boolean,
+        require: true
+    },
+    jobScore:  {
+        type: Number,
+        require: false
+    },
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
+
+const App: App = model("App", appSchema);
+
+module.exports = App;
