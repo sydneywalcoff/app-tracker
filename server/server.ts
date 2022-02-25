a4bd69acd4b4c3a944bc1d8bf092fd1ce653094a
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-25 14:56:38 -0800

    whoops forgot this was a typescript project; uploaded typescript and types packages for express

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`listening at localhost:${PORT}`);
+    })
+});
