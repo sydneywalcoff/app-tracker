3e84c9a251b8591b64d9a502c6b5c86b479ebdce
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-28 11:17:49 -0800

    established apollo provider to connect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,42 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+import "./App.css";
 
 // components
-import Header from './components/Header';
-import NavBar from './components/NavBar';
-import SingleApp from './components/SingleApp';
-import TrackerForm from './components/TrackerForm';
-import TrackerTable from './components/TrackerTable';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import SingleApp from "./components/SingleApp";
+import TrackerForm from "./components/TrackerForm";
+import TrackerTable from "./components/TrackerTable";
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: cache,
+});
 
 function App() {
-
   return (
-    <div className='h-screen'>
-      <Header />
-      <section className='h-screen flex'>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<TrackerForm/>} />
-          <Route path="applied" element ={<TrackerTable/>} />
-          <Route path="applied/:jobId" element={<SingleApp/>}/>
-        </Routes>
-      </section>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="h-screen">
+        <Header />
+        <section className="h-screen flex">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<TrackerForm />} />
+            <Route path="applied" element={<TrackerTable />} />
+            <Route path="applied/:jobId" element={<SingleApp />} />
+          </Routes>
+        </section>
+      </div>
+    </ApolloProvider>
   );
 }
 
