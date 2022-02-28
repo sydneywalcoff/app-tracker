715f1b148a766c72003b22633cd14375ef9ce1cd
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-28 11:40:20 -0800

    tracker table rendering dynamically from db

--- client/src/components/TrackerTable.tsx
+++ client/src/components/TrackerTable.tsx
@@ -1,69 +1,109 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { getJobs } from '../utils/localStorage';
+import React from "react";
+import { Link } from "react-router-dom";
+import { getJobs } from "../utils/localStorage";
+import { useQuery } from "@apollo/client";
+import { QUERY_APPS } from "../utils/queries";
 
-import StageBadge from './StageBadge'
+import StageBadge from "./StageBadge";
 
 interface jobProp {
-    id: string,
-    jobTitle: string,
-    companyName: string,
-    jobDescription: string,
-    location: string,
-    stage: string,
-    dateApplied: string
+  _id: string;
+  jobTitle: string;
+  companyName: string;
+  jobDescription: string;
+  location: string;
+  stage: string;
+  dateApplied: string;
 }
 
 const TrackerTable = () => {
-    const jobs = getJobs();
-    return (
-        <div className="flex flex-col mx-auto my-8">
-            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-gray-50">
-                                <tr>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Applied</th>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Position</th>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Company</th>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Stage</th>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Location</th>
-                                    <th scope="col" className="relative px-6 py-3"></th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                                {jobs.map((job: jobProp) => (
-                                    <tr>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="flex items-center">
-                                                <p className="text-sm text-gray-900">{job.dateApplied}</p>
-                                            </div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <div className="text-sm text-gray-900">{job.jobTitle}</div>
-                                        </td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{job.companyName}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap">
-                                            <StageBadge 
-                                                stage={job.stage}
-                                            />
-                                        </td>
-                                        <td className ="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{job.location}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <Link to={job.id} className="text-indigo-600 hover:text-indigo-900">More</Link >
-                                        </td>
-                                    </tr>
-                                ))}
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+  const { data } = useQuery(QUERY_APPS);
+  console.log(data?.Apps)
 
+  const jobs: jobProp[] = data?.Apps || [];
+
+  return (
+    <div className="flex flex-col mx-auto my-8">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Date Applied
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Position
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Company
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Stage
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Location
+                  </th>
+                  <th scope="col" className="relative px-6 py-3"></th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {jobs.map((job: jobProp) => (
+                  <tr>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <p className="text-sm text-gray-900">
+                          {job.dateApplied}
+                        </p>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        {job.jobTitle}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {job.companyName}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <StageBadge stage={job.stage} />
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                      {job.location}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <Link
+                        to={job._id}
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        More
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TrackerTable;
