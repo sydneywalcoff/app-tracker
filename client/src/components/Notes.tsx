b92e005d0aa44170118b8822cd896a10ccc6ff2d
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-06 10:58:12 -0800

    starting to outline Notes component

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -1,8 +1,28 @@
 const Notes = () => {
+    const notes = [1,2,3,4,5];
+    const addNote = () => {
+        console.log('adding a new note')
+    };
     return (
         <>
             <h3 className="text-2xl mb-2">Notes</h3>
-            <textarea className="w-full h-full border-solid border-2"></textarea>
+            <div>
+                {notes.map((note, i) => (
+                    <div key={i}>
+                        <p>{note}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="flex flex-col">
+                <textarea className="w-full h-half border-solid border-2"></textarea>
+                <button
+              type="button"
+              className=" justify-center ml-auto my-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={addNote}
+            >
+              Add Note.
+            </button>
+            </div>
         </>
     );
 }
