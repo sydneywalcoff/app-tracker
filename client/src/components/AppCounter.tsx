a8a3204d8afa055608753e5af7bda1ef3e722c14
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-10 16:04:39 -0800

    add basic appCounter component

--- client/src/components/AppCounter.tsx
+++ client/src/components/AppCounter.tsx
@@ -1,7 +1,11 @@
 const AppCounter = () => {
-    return(
-        <div className="">
-            App Counter
+    const numOfApps= 8;
+
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col align-center p-4 my-4">
+            <h2 className="text-center text-sm">you've applied for</h2>
+            <h1 className="text-6xl text-center">{numOfApps}</h1>
+            <h2 className="text-center text-sm">applications today</h2>
         </div>
     );
 };
--- client/src/pages/Stats.tsx
+++ client/src/pages/Stats.tsx
@@ -2,8 +2,11 @@ import AppCounter from '../components/AppCounter'
 
 const Stats = () => {
     return (
-        <div className="flex justify-center">
-            <AppCounter />
+        <div className="p-6">
+            <h1 className='text-4xl underline text-gray-900 underline-gray-900 mb-4'>Stats</h1>
+            <div>
+                <AppCounter />
+            </div>
         </div>
     );
 };
