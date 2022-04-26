8fff355f07f4b76051bd6b55b3d1f441002f6a7d
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-25 19:32:38 -0700

    add bcrypt to hash user passwords

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,5 +1,6 @@
 import { Schema, model, Document, Model } from 'mongoose';
 import { AppDocument } from './App';
+const bcrypt = require('bcrypt');
 const App: AppDocument = require('./App');
 
 const userSchema = new Schema<UserDocument, Model<UserDocument>>({
@@ -27,6 +28,21 @@ const userSchema = new Schema<UserDocument, Model<UserDocument>>({
         getters: true
     }
 });
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
 
 export interface UserDocument extends Document {
     username: String,
