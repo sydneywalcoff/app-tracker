46045ce7cbb6313821fbf9e597f91c6177a60ee3
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-29 14:26:49 -0700

    basic login form functionality

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,23 @@
+import { ChangeEvent, useState } from 'react';
+
 const Login = () => {
+    const [loginForm, setLoginForm] = useState({
+        username: "",
+        password: '',
+        email: ''
+    })
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setLoginForm({
+            ...loginForm,
+            [name]: value
+        })
+    };
+
+    const handleLogin = (e:ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(loginForm)
+    };
 
     return(
         <section className="flex flex-col w-full">
@@ -7,21 +26,21 @@ const Login = () => {
                 <div className="flex pt-5">
                     <div className="flex flex-col w-1/2 p-5">
                         <h3 className='text-xl text-center'>Login</h3>
-                        <div className="flex flex-col">
-                            <label htmlFor="username">Username:</label>
-                            <input type="text" id="username" name="username" className="border border-gray-400"/>
+                        <form action="#" className="flex flex-col" onSubmit={handleLogin}>
+                            <label htmlFor="loginUsername">Username:</label>
+                            <input type="text" name="username" className="border border-gray-400" onChange={handleChange}/>
                             <label htmlFor="email">Password:</label>
-                            <input type="password" id="password" name="password" className="border border-gray-400"/>
-                        </div>
-                        <button className="border border-gray-400 mt-5 w-1/4 mx-auto">login</button>
+                            <input type="password" name="password" className="border border-gray-400" onChange={handleChange}/>
+                            <button type='submit' className="border border-gray-400 mt-5 w-1/4 mx-auto">login</button>
+                        </form>
                     </div>
                     <div className="flex flex-col w-1/2 p-5">
                         <h3 className='text-xl text-center'>Signup</h3>
                         <div className="flex flex-col">
                             <label htmlFor="email">Email:</label>
-                            <input type="text" id="email" name="email" className="border border-gray-400"/>
+                            <input type="text" name="email" className="border border-gray-400"/>
                             <label htmlFor="username">Username:</label>
-                            <input type="text" id="username" name="username" className="border border-gray-400"/>
+                            <input type="text" name="username" className="border border-gray-400"/>
                             <label htmlFor="email">Password:</label>
                             <input type="password" id="password" name="password" className="border border-gray-400"/>
                         </div>
