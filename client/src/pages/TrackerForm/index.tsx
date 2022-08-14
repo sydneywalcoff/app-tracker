c8f75befd1b7bb3efaa8d26d2df92e21fef0e31d
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-08-14 14:23:55 -0700

    add color variables to app css and style basic textArea

--- client/src/pages/TrackerForm/index.tsx
+++ client/src/pages/TrackerForm/index.tsx
@@ -8,6 +8,8 @@ import ContentContainer from "../../components/ContentContainer";
 import TextInput from "../../components/TextInput";
 import TextArea from "../../components/TextArea";
 
+import './assets/style.css';
+
 const TrackerFormPage = () => {
     const [formState, setFormState] = useState({
         jobTitle: "",
@@ -101,108 +103,107 @@ const TrackerFormPage = () => {
 
     return (
         <ContentContainer>
-            <>
-                <div className="flex-col p-6 mx-auto">
-                    <div className="mt-10 sm:mt-0">
-                        <div className="md:grid md:grid-cols-3 md:gap-6">
-                            <div className="md:col-span-1">
-                                <div className="px-4 sm:px-0">
-                                    <h3 className="text-lg font-medium leading-6 text-gray-900">
-                                        Job Information
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-600">
-                                        Use as much info as possible. All fields are required.
-                                    </p>
-                                </div>
+            <div className="flex-col p-6 mx-auto">
+                <div className="mt-10 sm:mt-0">
+                    <div className="md:grid md:grid-cols-3 md:gap-6">
+                        <div className="md:col-span-1">
+                            <div className="px-4 sm:px-0">
+                                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                                    Job Information
+                                </h3>
+                                <p className="mt-1 text-sm text-gray-600">
+                                    Use as much info as possible. All fields are required.
+                                </p>
                             </div>
-                            <div className="mt-5 md:mt-0 md:col-span-2">
-                                <form action="#" onSubmit={handleSubmit}>
-                                    <div className="shadow overflow-hidden sm:rounded-md">
-                                        <div className="px-4 py-5 bg-white sm:p-6">
-                                            <div className="grid grid-cols-6 gap-6">
-                                                <div className="col-span-6 sm:col-span-3">
-                                                    <TextInput 
-                                                        onChange={handleChange} 
-                                                        labelTitle="job title"
-                                                    />
-                                                </div>
-
-                                                <div className="col-span-6 sm:col-span-3">
-                                                    <TextInput 
-                                                        onChange={handleChange} 
-                                                        labelTitle="companyName"
-                                                    />
-                                                </div>
-
-                                                <div className="col-span-6 sm:col-span-4">
-                                                    <TextInput 
-                                                        onChange={handleChange} 
-                                                        labelTitle="location"
-                                                    />
-                                                </div>
-
-                                                <div className="col-span-6 sm:col-span-3">
-                                                    <label
-                                                        htmlFor="stage"
-                                                        className="block text-sm font-medium text-gray-700"
-                                                    >
-                                                        Stage
-                                                    </label>
-                                                    <select
-                                                        id="stage"
-                                                        name="stage"
-                                                        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                                        onBlur={handleChange}
-                                                    >
-                                                        <option value="preparing">Preparing</option>
-                                                        <option value="applied">Applied</option>
-                                                    </select>
-                                                </div>
-
-                                                <div className="col-span-6">
-                                                    <TextArea onChange={handleChange} labelText="job description"/>
-                                                </div>
-
-                                                <div className="col-span-6 inline-flex align-items">
-                                                    <label
-                                                        htmlFor="quick-apply"
-                                                        className="block text-sm font-medium text-gray-700"
-                                                    >
-                                                        Quick Apply?
-                                                    </label>
-                                                    <input
-                                                        type="checkbox"
-                                                        name="quick-apply"
-                                                        id="quick-apply"
-                                                        className="my-auto mx-2"
-                                                        onChange={handleCheckboxChange}
-                                                    />
-                                                    <TextInput 
-                                                        onChange={handleChange} 
-                                                        labelTitle="jobScan Score"
-                                                    />
-                                                </div>
+                        </div>
+                        <div className="mt-5 md:mt-0 md:col-span-2">
+                            <form action="#" onSubmit={handleSubmit}>
+                                <div className="shadow overflow-hidden sm:rounded-md">
+                                    <div className="px-4 py-5 sm:p-6">
+                                        <div className="grid grid-cols-6 gap-6">
+                                            <div className="col-span-6 sm:col-span-3">
+                                                <TextInput 
+                                                    onChange={handleChange} 
+                                                    labelTitle="job title"
+                                                />
                                             </div>
-                                            {inputError && (
-                                                <p className="text-red-700 mt-2">{inputError}</p>
-                                            )}
+
+                                            <div className="col-span-6 sm:col-span-3">
+                                                <TextInput 
+                                                    onChange={handleChange} 
+                                                    labelTitle="company name"
+                                                />
+                                            </div>
+
+                                            <div className="col-span-6 sm:col-span-4">
+                                                <TextInput 
+                                                    onChange={handleChange} 
+                                                    labelTitle="location"
+                                                />
+                                            </div>
+
+                                            <div className="col-span-6 sm:col-span-3">
+                                                <label
+                                                    htmlFor="stage"
+                                                    className="block text-sm font-medium text-gray-700"
+                                                >
+                                                    Stage
+                                                </label>
+                                                <select
+                                                    id="stage"
+                                                    name="stage"
+                                                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                    onBlur={handleChange}
+                                                >
+                                                    <option value="preparing">Preparing</option>
+                                                    <option value="applied">Applied</option>
+                                                </select>
+                                            </div>
+
+                                            <div className="col-span-6">
+                                                <TextArea onChange={handleChange} labelText="job description"/>
+                                            </div>
+
+                                            <div className="col-span-6 inline-flex align-items">
+                                                <label
+                                                    htmlFor="quick-apply"
+                                                    className="block text-sm font-medium text-gray-700"
+                                                >
+                                                    Quick Apply?
+                                                </label>
+                                                <input
+                                                    type="checkbox"
+                                                    name="quick-apply"
+                                                    id="quick-apply"
+                                                    className="my-auto mx-2"
+                                                    onChange={handleCheckboxChange}
+                                                />
+                                                <TextInput 
+                                                    onChange={handleChange} 
+                                                    labelTitle="jobScan Score"
+                                                />
+                                            </div>
                                         </div>
-                                        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                            <button
-                                                type="submit"
-                                                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                            >
-                                                Save
-                                            </button>
-                                        </div>
+                                        {inputError && (
+                                            <p className="text-red-700 mt-2">{inputError}</p>
+                                        )}
                                     </div>
-                                </form>
-                            </div>
+                                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                        <button
+                                            type="submit"
+                                            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        >
+                                            Save
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-                {/* How to Use Section */}
-            </>
+            </div>
+            {/* How to Use Section */}
+            
         </ContentContainer>
     );
 };
