f10a57bde63a7bfae39d7ce3426845db975b3c70
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-08-21 09:03:54 -0700

    restyled unclicked checkbox

--- client/src/components/Checkbox/index.tsx
+++ client/src/components/Checkbox/index.tsx
@@ -4,26 +4,25 @@ import './assets/style.css';
 
 interface CheckboxPropsI {
     onChange: ChangeEventHandler<HTMLInputElement>
-    classes?: String,
 }
 
-const Checkbox = ({onChange, classes}: CheckboxPropsI) => {
+const Checkbox = ({onChange}: CheckboxPropsI) => {
     return(
-        <>
+        <div className="checkbox-div">
             <label
                 htmlFor="quick-apply"
                 className="block"
             >
                 Quick Apply?
             </label>
+            <span className="checkbox ml-2 drop-shadow-md"></span>
             <input
                 type="checkbox"
                 name="quick-apply"
                 id="quick-apply"
-                className={`mx-2 ${classes}`}
                 onChange={onChange}
             />
-        </>
+        </div>
     )
 };
 
