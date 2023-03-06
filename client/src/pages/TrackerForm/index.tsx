6c59c42366d8aca58141177fa493fe5911308392
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-03-05 21:03:42 -0800

    styling tracker form

--- client/src/pages/TrackerForm/index.tsx
+++ client/src/pages/TrackerForm/index.tsx
@@ -137,7 +137,7 @@ const TrackerFormPage = () => {
                             <h1>The Tracker</h1>
                             <form action="#" className="form" onSubmit={handleSubmit}>
                                 <div className="overflow-hidden sm:rounded-md">
-                                    <div className="px-4 py-5 sm:p-6 flex flex-col shadow">
+                                    <div className="px-4 py-5 sm:p-6 flex flex-col shadow w-full h-full justify-center">
                                         <div className="grid grid-cols-6 gap-6">
                                             <div className="col-span-6 sm:col-span-3 flex flex-col">
                                                 <TextInput 
