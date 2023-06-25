5d8c54480a9cffcc712ab8957db24155cfbf7078
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-06-25 11:10:51 -0700

    split sectionContainer from ContentContainer component

--- client/src/pages/TrackerForm/index.tsx
+++ client/src/pages/TrackerForm/index.tsx
@@ -5,6 +5,7 @@ import { ADD_APP } from "../../utils/mutations";
 import Auth from '../../utils/auth';
 
 import ContentContainer from "../../components/ContentContainer";
+import SectionContainer from "../../components/SectionContainer";
 import TextInput from "../../components/TextInput";
 import TextArea from "../../components/TextArea";
 import Button from '../../components/Button';
@@ -100,104 +101,102 @@ const TrackerFormPage = () => {
     };
 
     return (
-        <ContentContainer>
+        <SectionContainer>
             <div className="flex-col tracker">
                 <div className="mt-10 sm:mt-0">
-                    <div className="flex">
-                        <div className="about">
-                            
-                        </div>
-                        <div className="form-container flex">
-                            <h1>The Tracker</h1>
-                            <form action="#" className="form" onSubmit={handleSubmit}>
-                                <div className="overflow-hidden sm:rounded-md">
-                                    <div className="px-4 py-5 sm:p-6 flex flex-col shadow w-full h-full justify-center">
-                                        <div className="grid grid-cols-6 gap-6">
-                                            <div className="col-span-6 sm:col-span-3 flex flex-col">
-                                                <TextInput
-                                                    onChange={handleChange}
-                                                    name="job-title"
-                                                    labelTitle="Job title"
-                                                />
+                    <ContentContainer>
+                        <div className="flex">
+                            <div className="about">
+                        
+                            </div>
+                            <div className="form-container flex">
+                                <h1>The Tracker</h1>
+                                <form action="#" className="form" onSubmit={handleSubmit}>
+                                    <div className="overflow-hidden sm:rounded-md">
+                                        <div className="px-4 py-5 sm:p-6 flex flex-col shadow w-full h-full justify-center">
+                                            <div className="grid grid-cols-6 gap-6">
+                                                <div className="col-span-6 sm:col-span-3 flex flex-col">
+                                                    <TextInput
+                                                        onChange={handleChange}
+                                                        name="job-title"
+                                                        labelTitle="Job title"
+                                                    />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3 flex flex-col">
+                                                    <TextInput
+                                                        onChange={handleChange}
+                                                        name="company-name"
+                                                        labelTitle="Company name"
+                                                    />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3 flex-col flex">
+                                                    <TextInput
+                                                        onChange={handleChange}
+                                                        name="location"
+                                                        labelTitle="Location"
+                                                    />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3 flex-col flex">
+                                                    <TextInput
+                                                        onChange={handleChange}
+                                                        name="job-score"
+                                                        labelTitle="JobScan score"
+                                                    />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3">
+                                                    <Dropdown onChange={handleChange} />
+                                                </div>
+                                                <div className="col-span-6">
+                                                    <TextArea onChange={handleChange} name="job-description" labelText="Job description" />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3 inline-flex align-items">
+                                                    <Checkbox onChange={handleCheckboxChange} classes="ml-3" />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3 inline-flex "><Button text="Save" classes="primary" type="submit" /></div>
                                             </div>
-
-                                            <div className="col-span-6 sm:col-span-3 flex flex-col">
-                                                <TextInput
-                                                    onChange={handleChange}
-                                                    name="company-name"
-                                                    labelTitle="Company name"
-                                                />
-                                            </div>
-
-                                            <div className="col-span-6 sm:col-span-3 flex-col flex">
-                                                <TextInput
-                                                    onChange={handleChange}
-                                                    name="location"
-                                                    labelTitle="Location"
-                                                />
-                                            </div>
-
-                                            <div className="col-span-6 sm:col-span-3 flex-col flex">
-                                                <TextInput
-                                                    onChange={handleChange}
-                                                    name="job-score"
-                                                    labelTitle="JobScan score"
-                                                />
-                                            </div>
-
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <Dropdown onChange={handleChange} />
-                                            </div>
-
-                                            <div className="col-span-6">
-                                                <TextArea onChange={handleChange} name="job-description" labelText="Job description" />
-                                            </div>
-
-                                            <div className="col-span-6 sm:col-span-3 inline-flex align-items">
-                                                <Checkbox onChange={handleCheckboxChange} classes="ml-3" />
-                                            </div>
-                                            <div className="col-span-6 sm:col-span-3 inline-flex "><Button text="Save" classes="primary" type="submit" /></div>
+                                            {inputError && (
+                                                <p className="text-red-700 mt-2">{inputError}</p>
+                                            )}
                                         </div>
-                                        {inputError && (
-                                            <p className="text-red-700 mt-2">{inputError}</p>
-                                        )}
                                     </div>
+                                </form>
+                            </div>
+                        </div>
+                    </ContentContainer>
+                    <ContentContainer>
+                        <div className="how-to">
+                            <h1>How to Use</h1>
+                            <div className="use-list-container">
+                                <div className="use-tip">
+                                    <h2 className="use-title">Basic Info</h2>
+                                    <div className="use-answer"></div>
                                 </div>
-                            </form>
-                        </div>
-                    </div>
-                    <div className="how-to">
-                        <h1>How to Use</h1>
-                        <div className="use-list-container">
-                            <div className="use-tip">
-                                <h2 className="use-title">Basic Info</h2>
-                                <div className="use-answer"></div>
-                            </div>
-                            <div className="use-tip">
-                                <h2 className="use-title">Location</h2>
-                                <div className="use-answer"></div>
-                            </div>
-                            <div className="use-tip">
-                                <h2 className="use-title">Stage</h2>
-                                <div className="use-answer"></div>
-                            </div>
-                            <div className="use-tip">
-                                <h2 className="use-title">JobScan Score</h2>
-                                <div className="use-answer"></div>
-                            </div>
-                            <div className="use-tip">
-                                <h2 className="use-title">Job Description</h2>
-                                <div className="use-answer"></div>
-                            </div>
-                            <div className="use-tip">
-                                <h2 className="use-title">Quick Apply</h2>
-                                <div className="use-answer"></div>
+                                <div className="use-tip">
+                                    <h2 className="use-title">Location</h2>
+                                    <div className="use-answer"></div>
+                                </div>
+                                <div className="use-tip">
+                                    <h2 className="use-title">Stage</h2>
+                                    <div className="use-answer"></div>
+                                </div>
+                                <div className="use-tip">
+                                    <h2 className="use-title">JobScan Score</h2>
+                                    <div className="use-answer"></div>
+                                </div>
+                                <div className="use-tip">
+                                    <h2 className="use-title">Job Description</h2>
+                                    <div className="use-answer"></div>
+                                </div>
+                                <div className="use-tip">
+                                    <h2 className="use-title">Quick Apply</h2>
+                                    <div className="use-answer"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </ContentContainer>
                 </div>
             </div>
-        </ContentContainer>
+        </SectionContainer>
     );
 };
 
