7f18f9d56d4d66d5e57f0fa0d579f890c44dcd12
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-06-24 18:07:47 -0700

    refactoring and restyling a little

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -15,7 +15,7 @@ const Header = () => {
     }
 
     return(
-        <header className="flex text-white bg-blue-900 p-4 justify-between drop-shadow-md">
+        <header className="flex text-white p-4 justify-between drop-shadow-md">
             <Logo />
             <img src={ isMobileMenuOpen ? OpenNavMenu : ClosedNavMenu } className='mobileNavMenu' onClick={navMenuClickHandler} alt=''/>
             <NavBar isMobileMenuOpen={isMobileMenuOpen}/>
