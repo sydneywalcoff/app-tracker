f6c09b03e2837323527440486994b7e37216292e
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-07-23 19:42:25 -0700

    styling to text-area container

--- client/src/components/TextArea/index.tsx
+++ client/src/components/TextArea/index.tsx
@@ -22,7 +22,7 @@ const TextArea = ({onChange, labelText, name, classes }: TextAreaPropsI) => {
                 name={name}
                 rows={5}
                 id={name}
-                className={`mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full drop-shadow-md ${classes}`}
+                className={`mt-1 py-2 px-3 block w-full drop-shadow-md ${classes}`}
                 onChange={onChange}
             />
         </>
