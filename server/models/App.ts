b5c4f25e8d7a1511519e4280cba557f5d53c3558
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-10-25 14:34:00 -0700

    remove getter from server App model to pass the date as a str

--- server/models/App.ts
+++ server/models/App.ts
@@ -35,16 +35,16 @@ const appSchema = new Schema<AppDocument, Model<AppDocument>>({
     dateApplied: {
         type: Date,
         default: Date.now,
-        get: timeStamp => dateFormat(timeStamp),
+        // get: timeStamp => dateFormat(timeStamp),
     },
     lastUpdated: {
         type: Date,
-        get: timeStamp => {
-            if(!timeStamp) {
-                return ''
-            }
-            return dateFormat(timeStamp)
-        },
+        // get: timeStamp => {
+        //     if(!timeStamp) {
+        //         return ''
+        //     }
+        //     return dateFormat(timeStamp)
+        // },s
     },
     notes: [noteSchema], 
     link: {
