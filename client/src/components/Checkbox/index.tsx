eb7a6165a94b36000d34fa604853b0e8a3a04721
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-11-20 19:13:13 -0800

    started refactoring checkbox

--- client/src/components/Checkbox/index.tsx
+++ client/src/components/Checkbox/index.tsx
@@ -5,22 +5,23 @@ import './assets/style.css';
 interface CheckboxPropsI {
     onChange: ChangeEventHandler<HTMLInputElement>
     classes?: string;
+    name: string;
 }
 
-const Checkbox = ({onChange, classes}: CheckboxPropsI) => {
+const Checkbox = ({onChange, classes, name}: CheckboxPropsI) => {
     
     return(
         <div className="checkbox-div">
             <label
-                htmlFor="quick-apply"
+                htmlFor={name}
                 className="block"
             >
                 Quick Apply?
             </label>
             <input
                 type="checkbox"
-                name="quick-apply"
-                id="quick-apply"
+                name={name}
+                id={name}
                 onChange={onChange}
             />
             <span className={`checkbox-custom drop-shadow-md ${classes ? classes : ''}`}></span>
