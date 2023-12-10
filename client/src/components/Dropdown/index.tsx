a778b3e2d71ffe8f5d83992284f1976df73c0350
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-12-10 15:14:34 -0800

    add optional options value to type and refactored Dropdown

--- client/src/components/Dropdown/index.tsx
+++ client/src/components/Dropdown/index.tsx
@@ -4,9 +4,10 @@ import './assets/style.css';
 
 interface DropdownPropsI {
     onChange: ChangeEventHandler<HTMLSelectElement>
+    options?: string[]
 }
 
-const Dropdown = ({ onChange }: DropdownPropsI) => {
+const Dropdown = ({ onChange, options }: DropdownPropsI) => {
     return (
         <>
             <label
@@ -20,9 +21,12 @@ const Dropdown = ({ onChange }: DropdownPropsI) => {
                 name="stage"
                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 onBlur={onChange}
-            >
+            >{ options ? options.map(option => (
+                <option value={option}>{option}</option>
+            )) : (<>
                 <option value="preparing">Preparing</option>
                 <option value="applied">Applied</option>
+                </>)}
             </select>
         </>
     );
