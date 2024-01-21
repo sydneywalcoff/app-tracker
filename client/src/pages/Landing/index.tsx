343449dce94fa30d3bd179d2c8cf8080622b1da2
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-01-20 19:21:43 -0800

    add landing page image, and started landing page

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -2,13 +2,22 @@
 
 import ContentContainer from '../../components/ContentContainer';
 
+import LandingHeroImg from '../../assets/landing-hero.svg';
+
 import './assets/style.css';
 
 const LandingPage = () => {
 
     return (
         <ContentContainer className='gray-bg'>
-           <></>
+            <div className="flex hero">
+                <div className="img-container">
+                    <img src={LandingHeroImg} alt="" />
+                    </div>
+                <div className="text-container">
+                    <h1>JOB HUNTING SUCKS</h1>
+                </div>
+            </div>
         </ContentContainer>
     )
 };
