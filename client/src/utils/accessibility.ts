2dffa24781e37e3e0e53fee8a2becdcc572953ef
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-02-19 15:33:59 -0800

    add up/down arrow keys to a11y util

--- client/src/utils/accessibility.ts
+++ client/src/utils/accessibility.ts
@@ -1,4 +1,7 @@
-export default {
+const Accessibility = {
     toggleKeys: ['Enter', 'Space'],
     closeKeys: ['Escape'],
+    upKeys: ['ArrowUp'],
+    downKeys: ['ArrowDown'],
 };
+export default Accessibility
