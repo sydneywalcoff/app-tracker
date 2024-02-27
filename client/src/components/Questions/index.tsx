d76711535a43b1e1b57880ac7afb0d6f5906f662
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-02-26 20:46:18 -0800

    basic question skeleton

---
+++ client/src/components/Questions/index.tsx
@@ -0,0 +1,14 @@
+const Questions = () => {
+    const questionList = ['What is the breakdown of the team and who does what?', 'What are you most excited about having a new person in this role?', 'What is your biggest pain point? How will this role alleviate that?', 'What advice would you give someone through the rest of the interviewing process?', 'What is the rest of the hiring process?'];
+
+    return (
+        <>
+            <h3 className="text-2xl mb-2">Questions</h3>
+            <ul className="px-5">
+                {questionList && questionList.map((question, i) => <li className="list-disc mb-3" key={`question${i}`}>{question}</li>)}
+            </ul>
+        </>
+    );
+};
+
+export default Questions;
