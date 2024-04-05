8908402f09c3725e574cada31a5ed44e2d4c0402
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-04-05 15:30:42 -0700

    add QuestionDocu ment to the index and reference in the App

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,9 +1,11 @@
 const App = require('./App');
 const User = require('./User');
+const Question = require('./Question');
 
 import { NoteDocument } from './Note';
 import { AppDocument } from './App';
+import { QuestionDocument } from './Question';
 
-export { NoteDocument, AppDocument }
+export { NoteDocument, AppDocument, QuestionDocument }
 
-module.exports = { App, User }; 
+module.exports = { App, User, Question }; 
