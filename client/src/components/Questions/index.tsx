286268527aaa304de1bcd27f2301d8f0676d6574
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-04-15 16:02:09 -0700

    fix small screen question-container and reformat

--- client/src/components/Questions/index.tsx
+++ client/src/components/Questions/index.tsx
@@ -18,14 +18,16 @@ const Questions = ({ questions: questionList }: IQuestionParams) => {
     return (
         <div className='questions'>
             <h3 className="text-2xl mb-2">Questions</h3>
-            <ul className="px-5">
+            <ul className="px-5 flex justify-between flex-col">
                 {questionList && questionList.map(question => {
                     return (
-                        <li className="list-disc mb-3 flex" key={`${question._id}`}>
-                            <p>{question.questionText}</p>
-                            <div className="buttons flex">
-                                <img src={editBtn} alt="" />
-                                <img src={deleteBtn} alt="" />
+                        <li className="list-disc" key={`${question._id}`}>
+                            <div className="li-content mb-3 flex">
+                                <p className='flex items-center'>{question.questionText}</p>
+                                <div className="buttons flex ml-1">
+                                    <img src={editBtn} alt="" />
+                                    <img src={deleteBtn} alt="" />
+                                </div>
                             </div>
                         </li>
                     );
