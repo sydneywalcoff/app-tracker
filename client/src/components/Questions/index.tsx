4a6aa2f756b3900feca616524d405672016c4e3b
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-04-15 16:06:22 -0700

    question component styling

--- client/src/components/Questions/index.tsx
+++ client/src/components/Questions/index.tsx
@@ -18,11 +18,11 @@ const Questions = ({ questions: questionList }: IQuestionParams) => {
     return (
         <div className='questions'>
             <h3 className="text-2xl mb-2">Questions</h3>
-            <ul className="px-5 flex justify-between flex-col">
+            <ul className="px-5 flex flex-col">
                 {questionList && questionList.map(question => {
                     return (
                         <li className="list-disc" key={`${question._id}`}>
-                            <div className="li-content mb-3 flex">
+                            <div className="li-content mb-3 flex justify-between">
                                 <p className='flex items-center'>{question.questionText}</p>
                                 <div className="buttons flex ml-1">
                                     <img src={editBtn} alt="" />
