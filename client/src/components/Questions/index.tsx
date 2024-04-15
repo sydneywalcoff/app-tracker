59425de2dfc2230eb6bf13c9ad945dc0042899f1
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-04-15 16:18:07 -0700

    refactor and start adding functionality

--- client/src/components/Questions/index.tsx
+++ client/src/components/Questions/index.tsx
@@ -1,17 +1,43 @@
+import { useState } from 'react';
+
 import './assets/style.css'
 import editBtn from '../../assets/edit.svg';
 import deleteBtn from '../../assets/trash.svg';
 
 interface IQuestionParams {
-    questions: Question[];
+    questions: IQuestion[];
 }
 
-type Question = {
+interface IQuestion {
     questionText: String;
     _id: String;
     lastUpdated: String;
     roleTag: String;
 }
+
+const Question = (question: IQuestion) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        console.log('edit')
+    };
+
+    const handleTrashClick = () => {
+        console.log('trash')
+    };
+
+    return (
+        <li className="list-disc" key={`${question._id}`}>
+            <div className="li-content mb-3 flex justify-between">
+                <p className='flex items-center'>{question.questionText}</p>
+                <div className="buttons flex ml-1">
+                    <button onClick={handleEditClick}><img src={editBtn} alt="" /></button>
+                    <button onClick={handleTrashClick}><img src={deleteBtn} alt="" /></button>
+                </div>
+            </div>
+        </li>
+    );
+};
 
 const Questions = ({ questions: questionList }: IQuestionParams) => {
 
@@ -19,19 +45,7 @@ const Questions = ({ questions: questionList }: IQuestionParams) => {
         <div className='questions'>
             <h3 className="text-2xl mb-2">Questions</h3>
             <ul className="px-5 flex flex-col">
-                {questionList && questionList.map(question => {
-                    return (
-                        <li className="list-disc" key={`${question._id}`}>
-                            <div className="li-content mb-3 flex justify-between">
-                                <p className='flex items-center'>{question.questionText}</p>
-                                <div className="buttons flex ml-1">
-                                    <button><img src={editBtn} alt="" /></button>
-                                    <button><img src={deleteBtn} alt="" /></button>
-                                </div>
-                            </div>
-                        </li>
-                    );
-                })}
+                {questionList && questionList.map(question => Question(question))}
             </ul>
         </div>
     );
