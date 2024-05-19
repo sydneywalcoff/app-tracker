f5b0a53d9d7701bf55646a02aff9acf406af2135
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-05-18 19:10:35 -0700

    small style refactor

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -42,8 +42,14 @@ const DashboardPage = () => {
         <ContentContainer className='dashboard flex'>
             <>
                 <div className="main-container w-2/3">
-                    <div className="preparing">
-                        <AppTable apps={apps} />
+                    <div className="top-section flex">
+                        <div className="preparing w-1/2">
+                            <AppTable apps={apps} />
+                        </div>
+                        <div className="stats ml-4 shadow-md w-1/2 rounded-lg p-4">
+                            <h4>Today</h4>
+                            <p><span></span> Apps Applied to</p>
+                        </div>
                     </div>
                     <div className="all-apps mt-12">
                         <SearchBar searchText={searchText} setSearchText={setSearchText} />
