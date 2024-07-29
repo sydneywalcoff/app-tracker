10a7aac2d2bfdd3f55ab4961250ffd8a5fc474c0
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-07-29 11:10:03 -0700

    refactor AppTable to create parent div and move page counter from table el to parent div

--- client/src/components/AppTable/index.tsx
+++ client/src/components/AppTable/index.tsx
@@ -77,21 +77,23 @@ const AppTable = (params: AppTableI) => {
     }
 
     return (
-        <table className="w-full divide-y flex flex-col app-table">
-            <thead className="w-full">
-                <tr className="section-titles flex justify-between p-1 w-full">
-                    <th className='date-added title'><p>Date</p></th>
-                    <th className='job-title title'><p>Job Title</p></th>
-                    <th className='company title'><p>Company</p></th>
-                    <th className='stage title'><p>Stage</p></th>
-                    <th className='location title'><p>Location</p></th>
-                    <th className='ATS-score title'><p>ATS Score</p></th>
-                    <th className="spacer title"></th>
-                </tr>
-            </thead>
-            <tbody className="body w-full">
-                {jobs.map(app => <AppItem _id={app._id} dateAdded={app.dateApplied} jobTitle={app.jobTitle} company={app.companyName} stage={app.status} location={app.location} jobScore={app.jobScore} key={app._id}/>)}
-            </tbody>
+        <div className="app-table">
+            <table className="w-full divide-y flex flex-col">
+                <thead className="w-full">
+                    <tr className="section-titles flex justify-between p-1 w-full">
+                        <th className='date-added title'><p>Date</p></th>
+                        <th className='job-title title'><p>Job Title</p></th>
+                        <th className='company title'><p>Company</p></th>
+                        <th className='stage title'><p>Stage</p></th>
+                        <th className='location title'><p>Location</p></th>
+                        <th className='ATS-score title'><p>ATS Score</p></th>
+                        <th className="spacer title"></th>
+                    </tr>
+                </thead>
+                <tbody className="body w-full">
+                    {jobs.map(app => <AppItem _id={app._id} dateAdded={app.dateApplied} jobTitle={app.jobTitle} company={app.companyName} stage={app.status} location={app.location} jobScore={app.jobScore} key={app._id}/>)}
+                </tbody>
+            </table>
             {numJobs > 10 &&
                 (
                     <div className="page-counter-wrap">
@@ -119,7 +121,7 @@ const AppTable = (params: AppTableI) => {
                         </div>
                     </div>
                 )}
-        </table>
+        </div>
     );
 };
 
