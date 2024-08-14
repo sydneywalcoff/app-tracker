94e7672385fefd2d8da0574ae2ed0a932a5cd7b7
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-08-14 15:18:30 -0700

    add ability to select new radio btn option using the keyboard

--- client/src/components/RadioBtn/index.tsx
+++ client/src/components/RadioBtn/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, KeyboardEvent } from "react";
 
 import './assets/style.css';
+import Accessibility from "../../utils/accessibility";
 
 interface RadioBtnPropsI {
     label: string;
@@ -11,6 +12,7 @@ interface RadioBtnPropsI {
 const RadioBtn = (params: RadioBtnPropsI) => {
     const { label, onStyleChange, selected } = params;
     const [isSelected, setSelected] = useState(false);
+    const radioBtnRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => { 
         selected === label ? setSelected(true) : setSelected(false)
@@ -18,10 +20,21 @@ const RadioBtn = (params: RadioBtnPropsI) => {
 
     const handleClick = () => {
         onStyleChange(label);
+        radioBtnRef.current?.focus()
+    };
+
+    const handleRadioBtnKeyDown = (e: KeyboardEvent) => {
+        const { code } = e;
+        const { toggleKeys } = Accessibility;
+
+        if(toggleKeys.includes(code)) {
+            handleClick();
+            return;
+        }
     };
 
     return (
-        <div className="radio-btn-container flex" onClick={handleClick}>
+        <div className="radio-btn-container flex" onClick={handleClick} onKeyDown={handleRadioBtnKeyDown}>
             <div role="radio" aria-checked={isSelected} aria-labelledby={`radio-label-${label}`} tabIndex={0} className={`radio-btn shadow-sm ${isSelected ? 'selected' : ''}`}></div>
             <p className="label" id={`radio-label-${label}`}>{label}</p>
         </div>
