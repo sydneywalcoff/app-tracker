b3680cfca03c8ff764fd8cbd914f215c81baa35e
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-11-06 16:09:23 -0800

    updated enough that LP looks normal, need to figure out other routes

--- vite-client/src/main.tsx
+++ vite-client/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import LandingPage from './pages/Landing/index.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
