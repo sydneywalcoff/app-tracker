67edce04b272666afea0666257a71f71ca65ffa8
Sydney Walcoff <sydney.walcoff@gmail.com>
2025-01-21 17:17:09 -0800

    condense directory into file

---
+++ client/src/components/StageDropdown.tsx
@@ -0,0 +1,29 @@
+import StageBadge from "./StageBadge";
+
+import { JobProp } from "../types/global.types";
+
+import './assets/style.css';
+
+
+interface StageDropdownPropsI {
+    onStageChange: (newStage: string, job?: (JobProp | undefined)) => Promise<void> | void;
+    job?: JobProp | undefined;
+    selectedStage: string;
+    options: Array<string>;
+    hideLabel?: Boolean;
+    classes?: string;
+    hideArrow?: Boolean;
+}
+
+
+import Dropdown from './Dropdown';
+
+const StageDropdown = ({ onStageChange, selectedStage, options, hideLabel, classes, hideArrow }: StageDropdownPropsI) => {
+    let formattedClasses = 'stage-dropdown-container ' + classes;
+
+    return (
+        <Dropdown onChange={onStageChange} selectedOption={selectedStage} options={options} hideLabel={hideLabel} label="stage" id="stage-dropdown" classes={formattedClasses} OptionWrapperEl={StageBadge} hideArrow={hideArrow}/>
+    )
+};
+
+export default StageDropdown;
